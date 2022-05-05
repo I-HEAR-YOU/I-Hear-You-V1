7764ef4aba560e8c9f7c0ae9d66f8b7f1f813df2
Onyi (Dre) Anozie <88848554+onyianozie@users.noreply.github.com>
2022-05-05 14:58:07 -0400

    Worked on splash page, added routes, and links, basic css

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import{ Button } from './Button';
+
+function Navbar() {
+  const [click, setClikck] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClikck (!click);
+  const closeMobileMenu = () => setClikck (false);
+
+  const showButton = () =>{
+      if(window.innerWidth <= 960){
+          setButton(false);
+      }else{
+          setButton(true);
+      }
+  };
+
+  window.addEventListener('resize', showButton)
+
+  return (
+    <div>
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        IHearYou <i className="fa-solid fa-mobile-screen"></i>
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'>Enter</Button>}
+                </div>
+            </nav>
+        </>
+    </div>
+  )
+}
+
+export default Navbar
